64c38a847049a0a04328f572c15fe3f5dfd89016
Binada-SH <binadasasmika@gmail.com>
2025-05-30 22:36:33 +0530

    Navbar ui updates

--- serendib_crafts/src/Components/Navbar/Navbar.jsx
+++ serendib_crafts/src/Components/Navbar/Navbar.jsx
@@ -3,8 +3,8 @@ import logo from "../../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <div className='m-auto w-300 max-w-2560  fixed flex items-center justify-between px-4 py-2 bg-gray-200 rounded-4xl h-15'>
-        <img className='w-20 ml-30' src={logo} alt="site logo"/>
+    <div className='m-auto w-300 max-w-2560  fixed flex items-center justify-between px-4 py-2 bg-gray-100 rounded-4xl h-15'>
+        <img className='w-20 ml-20' src={logo} alt="site logo"/>
         <ul className='flex items-center gap-14 font-[jura]'>
             <li><a href="">Home</a></li>
             <li><a href="">About</a></li>
