ff4d9edd7dd304edca5983166f0abcdf195f50df
Binada-SH <binadasasmika@gmail.com>
2025-05-31 09:13:42 +0530

    Navbar redesign

--- serendib_crafts/src/Components/Navbar/Navbar.jsx
+++ serendib_crafts/src/Components/Navbar/Navbar.jsx
@@ -3,15 +3,17 @@ import logo from "../../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <div className='m-auto w-300 max-w-2560  fixed flex items-center justify-between px-4 py-2 bg-gray-100 h-15'>
-        <img className='w-20 ml-20' src={logo} alt="site logo"/>
-        <ul className='flex items-center gap-14 font-[jura]'>
-            <li><a href="">Home</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Shop</a></li>
-            <li><a href="">Contact</a></li>
+    <div className='w-full max-w-2560 h-18 top-0 left-0 fixed flex items-center justify-between px-4 py-2 bg-gray-100 '>
+        <ul className='flex items-center gap-8 font-[jura]'>
+            <li><a href="">HOME</a></li>
+            <li><a href="">ABOUT</a></li>
+            <li><a href="">SHOP</a></li>
+            <li><a href="">CONTACT</a></li>
         </ul>
-        <ul className='flex items-center gap-6 px-20'>
+        <h1>
+            <a className='text-[24px] font-[times] tracking-widest text-center' href="">HAYDEN</a>
+        </h1>
+        <ul className='flex items-center gap-6 px-1'>
             <li>
                 <a href="">
                     <i class="bi bi-heart"></i>
