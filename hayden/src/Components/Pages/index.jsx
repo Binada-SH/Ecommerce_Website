0432d3ce7a3fd8d4369307f82eb558b1aa03bc16
Binada-SH <binadasasmika@gmail.com>
2025-05-31 17:30:18 +0530

    UI updates

--- hayden/src/Components/Pages/index.jsx
+++ hayden/src/Components/Pages/index.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import Hero1 from "../../assets/hero-bg-1.webp"
+import React, { useEffect, useState } from 'react'
+import Hero1 from "../../assets/hero-bg-2.webp";
 
 const index = () => {
   return (
-    <div>
-        <img className='w-full h-full' src={Hero1} alt="" />
-      
+      <div className="absolute w-full h-screen overflow-hidden z-[-10] bg-[#f0f0f0] pt-40">
+        <img src={Hero1} alt="" />
     </div>
   )
 }
