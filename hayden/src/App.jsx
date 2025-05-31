8ec2712ecc0d9ffa345701c4c898ccb9d5405f9a
Binada-SH <binadasasmika@gmail.com>
2025-05-31 11:03:13 +0530

    Folder struct change

--- hayden/src/App.jsx
+++ hayden/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import './App.css';
 import Navbar from "./Components/Navbar/Navbar";
+import Index from './Components/Pages/index';
 
 function App() {
   return (
     <>
     <Navbar/>
+    <Index/>
     
     </>
   )
---
+++ hayden/src/Components/Pages/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero1 from "../../assets/hero-bg-1.webp"
+
+const index = () => {
+  return (
+    <div>
+        <img className='w-full h-full' src={Hero1} alt="" />
+      
+    </div>
+  )
+}
+
+export default index
