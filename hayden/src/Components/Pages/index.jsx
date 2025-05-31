b37851f1c2e2026d2901d98142ab387e4cf54d2e
Binada-SH <binadasasmika@gmail.com>
2025-05-31 21:32:57 +0530

    GSAP animations creating

--- hayden/src/Components/Pages/index.jsx
+++ hayden/src/Components/Pages/index.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Hero from "../../assets/hero-bg-2.webp";
 import Skin from "../../assets/product-1-1.webp";
 import Hair from "../../assets/product-7-1.webp";
 import Tool from "../../assets/product-15-2.webp";
+import gsap from "gsap";
 
 const categories = [
   {
@@ -30,13 +31,39 @@ const categories = [
 ]
 
 const Index = () => {
+  const fadeup = useRef(null);
+  useEffect (() => {
+  gsap.fromTo(
+    fadeup.current,
+    {
+      opacity: 0, y: 60
+    },
+    {
+      opacity: 1, y: 0, duration: 1.5, ease: "power2.out",
+    }
+  );
+},[]);
+
+  const fadeleft = useRef(null);
+  useEffect(() => {
+    gsap.fromTo(
+      fadeleft.current,
+      {
+        opacity: 0, x: 60
+      },
+      {
+        opacity: 1, x: 0, duration: 1.5, ease: "power2.out",
+      }
+    );
+  },[]);
+
   return (
     <div>
       <div className="relative w-full h-screen overflow-hidden z-[-10] bg-[#f0f0f0]">
-        <img src={Hero} alt="" className="absolute top-1 left-24 w-full h-full object-contain z-[-1] scale-75" />
-      <div className='absolute top-1/2 left-30 transform -translate-y-1/2 text-left'>
+        <img src={Hero} alt="" className="absolute top-1 left-24 w-full h-full object-contain z-[-1] scale-75" ref={fadeleft} style={{opacity: 0}}/>
+      <div className='absolute top-1/2 left-30 transform -translate-y-1/2 text-left' ref={fadeup} style={{opacity: 0}}>
       <h3 className='uppercase text-[#222222] font-[jura] font-bold'>- get the glow -</h3>
-        <h1 className='text-[60px] font-[kanit] text-[#000000] leading-tight items-center flex' >Be Your kind <br />of Beauty</h1>
+        <h1 className='text-[60px] font-[kanit] text-[#000000] leading-tight items-center flex'>Be Your kind <br />of Beauty</h1>
         <p className='w-120 mt-4 text-[16px] font-[now] font-extrabold text-[#4e4d4d] leading-widest'>Made using clean and non toxic substances. Our products are designed for everyone</p>
         <button className='mt-5 w-50 h-13 rounded-4xl bg-[#000000] tracking-wide font-[kanit] text-[#ffffff]'>Explore Our Products</button>
       </div>
@@ -44,7 +71,7 @@ const Index = () => {
       <div className='flex mt-13'>
         <h1 className='m-auto text-[30px] font-[kanit] tracking-wider'>Shop By Category</h1>
       </div>
-      <div className='m-auto justify-between mt-10 flex flex-cols-3 gap-10 px-20'>
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-10 px-20 mt-10'>
           {categories.map ((categories,index) => (
             <div key = {index} className='cursor-pointer'>
               <img className='w-80 h-90 hover:scale-105 transition-all duration-550' src={categories.src} alt="categoryImages" />
