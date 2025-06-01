b8a8e8c652e2fc7a9e26bcf39b42100c1faeb0d6
Binada-SH <binadasasmika@gmail.com>
2025-06-01 09:03:58 +0530

    Navbar responsive design

--- hayden/src/Components/Navbar/Navbar.jsx
+++ hayden/src/Components/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
     },[]);
   return (
     <div className='w-full max-w-2560 h-18 top-0 left-0 fixed flex items-center justify-between px-6 py-2 bg-[#ffffff] shadow-md' ref={fadedown} style={{opacity: 0}}>
-        <ul className='flex items-center gap-8 font-[jura] text-[14px] px-10'>
+        <ul className='hidden lg:flex items-center gap-8 font-[jura] text-[14px] px-10'>
             <li><a href="">HOME</a></li>
             <li><a href="">ABOUT</a></li>
             <li><a href="">SHOP</a></li>
@@ -26,13 +26,13 @@ const Navbar = () => {
         <h1>
             <a className='text-[24px] font-[times] tracking-widest' href="">HAYDEN</a>
         </h1>
-        <ul className='flex items-center gap-6 px-10'>
-            <li>
+        <ul className=' flex items-center gap-6 ld:px-10'>
+            <li className='hidden md:flex'>
                 <a href="">
                     <i class="bi bi-heart"></i>
                 </a>
             </li>
-            <li>
+            <li className='hidden md:flex'>
                 <a href="">
                     <i class="bi bi-person text-xl"></i>
                 </a>
